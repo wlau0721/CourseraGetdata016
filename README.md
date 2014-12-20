5eb1299996aad55a4f9929e9b4049ea3e7ba7559
Wilson <wlau0721@yahoo.com>
2014-12-19 18:16:41 -0800

    full R script generate tidy datasets

--- README.md
+++ README.md
@@ -2,3 +2,31 @@ CourseraGetdata016
 ==================
 
 This is the course project for Coursera Getting and Cleaning Data class Nov 2014 session.  
+
+The goal of this project is to create a tidy dataset from Human Activity Recognition Using Smartphones Dataset Version 1.0, which is available here - 
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+Overview
+========
+There are 3 files in this github repository: 
+README.md
+CookBook.txt
+run_analysis.R
+
+README.md
+========
+README.md explains the overall purpose of the work and the files included.
+
+CookBook.txt
+========
+CookBook.txt explains the tidy dataset created and how the analysis was done on the Smartphones datasets.
+
+run_analysis.R
+========
+run_analysis.R is the R script processing the Smartphones datasets and creating the tidy datasets.  To run run_analysis.R, the Smartphones datasets need to be stored at a directory named UCI which is in the directory as the run_analysis.R file.
+Please refer to CookBook.txt to find out the details of the tidy dataset. 
+
+
+License:
+========
+This project is licensed through Creative Common.  http://creativecommons.org/
--- run_analysis.R
+++ run_analysis.R
@@ -9,27 +9,47 @@ trains <- read.table("./UCI/train/subject_train.txt", header=F,sep="") #user ide
 testx <- read.table("./UCI/test/X_test.txt", header=F,sep="") #sensor data
 testy <- read.table("./UCI/test/Y_test.txt", header=F,sep="") #corresponding activity data related to sensor data
 tests <- read.table("./UCI/test/subject_test.txt", header=F,sep="") #user identifier from who sensor data is collected
+flabels <- read.table("./UCI/features.txt", header=F,sep="") #feature labels
+alabels <- read.table("./UCI/activity_labels.txt", header=F,sep="") #activity labels
 
-#merge sensor data, activity and subject into one data frame
+
+#1. Merges the training and the test sets to create one data set.
+##merge sensor data, activity and subject into one data frame
 train <- cbind(trainx,trainy,trains)
 test <- cbind(testx,testy,tests)
-
-#merge the training and testing datasets
+##Merges the training and the test sets to create one data set
 data <- rbind(train,test)
 
-#Extract mean and standard deviation measurements from original datasets
+#4. Add original dataset columne labels to the data frame
+featurelabels <- as.character(flabels[,2])  #assign sensor data feature's labels
+columnlabels <- c(featurelabels,"activity","subjectId") #aggregate activity and subjectid to feature labels
+colnames(data) <- columnlabels #assign column names to data frame
 
-#(optional) convert the activity label variables from numbers into factor strings
+#2. Extracts only the measurements on the mean and standard deviation for each measurement
+colindex <- c(grep("activity",colnames(data)),grep("subjectId",colnames(data)),grep("mean",colnames(data)),grep("std",colnames(data)))
+extractedData <- data[,colindex]
+colnames(extractedData) <- make.names(colnames(extractedData))
 
+#3. Uses descriptive activity names to name the activities in the data set
+activity <- extractedData$activity #extract activity column from extracted dataset
+for (i in seq(dim(alabels)[1])) {
+  matchindex <- activity == alabels[i,1]
+  activity[matchindex] <- as.character(alabels[i,2])
+}
+extractedData$activity <- activity
 
-#Add correct column names to the extracted dataset
-##use make.names function - https://class.coursera.org/getdata-016/forum/thread?thread_id=136
+#5. create tidy data group by activity, subject and both
+##tidy dataset showing the average group by activity label
+tidyDataActivity <- aggregate(extractedData,by = list(activitylabel = extractedData$activity),FUN=mean)
+tidyDataActivity$activity <- NULL  #remove duplicated column
+tidyDataActivity$subjectId <- NULL #remove duplicated column
 
+##tidy dataset showing the average group by subject
+tidyDataSubject <- aggregate(extractedData,by = list(subject = extractedData$subjectId),FUN=mean)
+tidyDataSubject$activity <- NULL  #remove duplicated column
+tidyDataSubject$subjectId <- NULL #remove duplicated column
 
-
-
-
-
-
-
-
+##tidy dataset showing the average group by activity activity and then subject
+tidyDataActivitySubject <- aggregate(extractedData,by = list(activitylabel = extractedData$activity,subject = extractedData$subjectId),FUN=mean)
+tidyDataActivitySubject$activity <- NULL  #remove duplicated column
+tidyDataActivitySubject$subjectId <- NULL #remove duplicated column
